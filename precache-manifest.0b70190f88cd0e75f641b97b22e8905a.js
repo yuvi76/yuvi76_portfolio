1ad44d96f7bb3274bac4322cc7cc938c330e072f
Yuvi <37393125+yuvi76@users.noreply.github.com>
2020-03-03 12:25:04 +0530

    Updates

---
+++ precache-manifest.0b70190f88cd0e75f641b97b22e8905a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8b2e9569e6b957913c63403b3c88ec",
+    "url": "/yuvi76_portfolio/index.html"
+  },
+  {
+    "revision": "3b11b06c30bee2268817",
+    "url": "/yuvi76_portfolio/static/css/main.3372929e.chunk.css"
+  },
+  {
+    "revision": "13eeddacf69cd9a9a5b5",
+    "url": "/yuvi76_portfolio/static/js/2.fad92a24.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/yuvi76_portfolio/static/js/2.fad92a24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b11b06c30bee2268817",
+    "url": "/yuvi76_portfolio/static/js/main.28626836.chunk.js"
+  },
+  {
+    "revision": "635b722120eeee80abef",
+    "url": "/yuvi76_portfolio/static/js/runtime-main.398d565d.js"
+  },
+  {
+    "revision": "f08384547028400008ce9a2a537aa199",
+    "url": "/yuvi76_portfolio/static/media/building-a-restful-API-with-node.js-and-mongoDB-Atlas.f0838454.jpg"
+  },
+  {
+    "revision": "cefcfb8b48059c06df4436fa7d98d026",
+    "url": "/yuvi76_portfolio/static/media/first-transaction-on-libra-blockchain.cefcfb8b.jpg"
+  },
+  {
+    "revision": "7dff62e141dd754edba22809c0391ba8",
+    "url": "/yuvi76_portfolio/static/media/voting1-blog-banner.7dff62e1.jpg"
+  },
+  {
+    "revision": "04e554b022cdfc784b5b2293792f1f55",
+    "url": "/yuvi76_portfolio/static/media/z.04e554b0.png"
+  }
+]);
