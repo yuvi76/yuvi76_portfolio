d10a5305e11e5471b8209ff307e1df982c6fc5a7
Yuvi <37393125+yuvi76@users.noreply.github.com>
2020-03-04 18:03:42 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,7 +22,7 @@ class Home extends Component {
                         <div className="vertical-center">
                             <div className="started-content">
                                 <h1 className="h-title">
-                                    Hello, I’m <strong>Yuvraj Upadhyay</strong> Blockchian Developer At Yudiz Solutions.
+                                    Hello, I’m <strong>Yuvraj Upadhyay</strong> Blockchain Developer At Yudiz Solutions.
                                 </h1>
                                 <div className="h-subtitle typing-subtitle">
                                     <Typing loop={true} hideCursor={true}>
