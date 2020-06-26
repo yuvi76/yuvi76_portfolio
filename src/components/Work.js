351b9115e4741913c0bc42283d7156a42d2d3edf
Yuvraj <yuvraj.u@yudiz.in>
2020-06-26 11:24:56 +0530

    Mebook Link added.

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,6 +3,7 @@ import b1 from '../images/voting1-blog-banner.jpg';
 import b2 from '../images/first-transaction-on-libra-blockchain.jpg';
 import b3 from '../images/building-a-restful-API-with-node.js-and-mongoDB-Atlas.jpg';
 import b4 from '../images/CryptoCurrency-Is-Proving-The-Best-Gauge-Of-The-Coronavirus-Outbreak-After-China-Stats-Shock.jpg';
+import b5 from '../images/Mebook-Logo.PNG';
 
 class Work extends Component {
 
@@ -27,7 +28,7 @@ class Work extends Component {
 										<div className="card_content">
 											<h2 className="card_title">Cryptocurrency Is Proving The Best Gauge Of The Coronavirus Outbreak After China Stats Shock.</h2>
 											<p className="card_text">China has declared that they're going to be cleaning banknot...</p>
-											<a href="https://blog.yudiz.com/cryptocurrency_proving_as_best_gauge_of_coronavirus_outbreak_post_china_stats_shock/" className="btn hover-animated" >
+											<a href="https://blog.yudiz.com/cryptocurrency_proving_as_best_gauge_of_coronavirus_outbreak_post_china_stats_shock/" className="btn hover-animated" target="_blank" rel="noopener noreferrer">
 												<span className="lnk">Read Blog </span>
 											</a>
 											<p className="card_text">2nd Mar 2020</p>
@@ -40,7 +41,7 @@ class Work extends Component {
 										<div className="card_content">
 											<h2 className="card_title">Building a RESTful API with Node.js and MongoDB Atlas.</h2>
 											<p className="card_text">In the domains of JavaScript and Node.js, MongoDB has set up itself as the go-to database. MongoDB A...</p>
-											<a href="https://blog.yudiz.com/building-a-restful-api-with-node-js-and-mongodb-atlas/" className="btn hover-animated" >
+											<a href="https://blog.yudiz.com/building-a-restful-api-with-node-js-and-mongodb-atlas/" className="btn hover-animated" target="_blank" rel="noopener noreferrer" >
 												<span className="lnk">Read Blog </span>
 											</a>
 											<p className="card_text">3rd Feb 2020</p>
@@ -53,7 +54,7 @@ class Work extends Component {
 										<div className="card_content">
 											<h2 className="card_title">First Transaction on Libra Blockchain.</h2>
 											<p className="card_text">Imagine that one of your friends lives across the sea & he/she requires some Libra coin, which you have in abundance. How cool it would be if you... </p>
-											<a href="https://blog.yudiz.com/first-transaction-on-libra-blockchain/" className="btn hover-animated" >
+											<a href="https://blog.yudiz.com/first-transaction-on-libra-blockchain/" className="btn hover-animated" target="_blank" rel="noopener noreferrer">
 												<span className="lnk">Read Blog </span>
 											</a>
 											<p className="card_text">26th Jun 2019</p>
@@ -66,7 +67,7 @@ class Work extends Component {
 										<div className="card_content">
 											<h2 className="card_title">Truly Decentralized Voting System powered by Blockchain (Version 1.0).</h2>
 											<p className="card_text">Think what would happen if the voting system is 100% trans...</p>
-											<a href="https://blog.yudiz.com/truly-decentralized-voting-system-powered-by-blockchain-version-1-0/" className="btn hover-animated" >
+											<a href="https://blog.yudiz.com/truly-decentralized-voting-system-powered-by-blockchain-version-1-0/" className="btn hover-animated" target="_blank" rel="noopener noreferrer" >
 												<span className="lnk">Read Blog </span>
 											</a>
 											<p className="card_text">23rd Apr 2019</p>
@@ -87,10 +88,20 @@ class Work extends Component {
 							<ul className="cards">
 								<li className="cards_item">
 									<div className="card">
-										<a href="https://cmbs.in" >
+										<a href="https://cmbs.in" target="_blank" rel="noopener noreferrer" >
 											<div className="card_image"><img src={b3} alt="blog3" /></div>
 											<div className="card_content">
 												<h2 className="card_title"><center>CMBS.IN</center></h2>
+											</div>
+										</a>
+									</div>
+								</li>
+								<li className="cards_item">
+									<div className="card">
+										<a href="http://mebookyuvi.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+											<div className="card_image"><img src={b5} alt="blog3" style={{height: "193px"}}/></div>
+											<div className="card_content">
+												<h2 className="card_title"><center>MeBook</center></h2>
 											</div>
 										</a>
 									</div>
